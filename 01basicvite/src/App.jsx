5f4acb266fe1315d9a40ec34010a02940ad85f1d
jakubbocek <bocek.j@gmail.com>
2024-01-23 09:12:30 +0100

    basic-react

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube";
+
+function App() {
+  const username = "jakub";
+  return (
+    <div>
+      <h1>Vite React App{2 + 2}</h1>
+      <h1>Vite React App {username}</h1>
+      <Youtube></Youtube>
+    </div>
+  );
+}
+
+export default App;
